e4fed3d673d1755aa6269031cbd9b19ce55112bd
Rego <pvrego@users.noreply.github.com>
2019-02-22 14:25:00 -0300

    Eclipse project.

---
+++ src/ProcAndShout-Rpi.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ProcAndShout-Rpi.cpp
+// Author      : Pablo Rego
+// Version     :
+// Copyright   : Copyright 2019
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
