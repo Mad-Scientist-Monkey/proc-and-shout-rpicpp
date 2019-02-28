7043691a2a82abc59116db03bec1c54d95c1716d
Rego <pvrego@users.noreply.github.com>
2019-02-28 15:02:40 -0300

    Main loop to accept.

--- src/ProcAndShout-Rpi.cpp
+++ src/ProcAndShout-Rpi.cpp
@@ -29,8 +29,10 @@ int main() {
 	addr_server.sin_port = htons (8888);
 
 	// Bind
-	if (bind(socket_server,(struct sockaddr *)&addr_server, sizeof (addr_server)) < 0)
-		cout << "## Error in bind." << endl;
+	if (bind(socket_server,(struct sockaddr *)&addr_server, sizeof (addr_server)) < 0){
+		perror ("## Error in bind. Application will now exit.");
+		return 1;
+	}
 	cout << "## Bind is ok." << endl;
 
 	// Listen
@@ -40,25 +42,26 @@ int main() {
 	// Accept incoming connection
 	cout << "## Waiting for incoming connections..." << endl;
 	int c = sizeof (struct sockaddr_in);
-	socket_client = accept (socket_server, (struct sockaddr *)&addr_client, (socklen_t*)&c);
-	if (socket_client < 0){
-		perror ("## Accept failed. Application will now exit.");
-		return 1;
+	while ((socket_client = accept (socket_server, (struct sockaddr *)&addr_client, (socklen_t*)&c))){
+		if (socket_client < 0){
+			perror ("## Accept failed. Application will now exit.");
+			return 1;
+		}
+
+		// Get the address from connected client
+		char *client_ip = inet_ntoa(addr_client.sin_addr);
+		int  client_port = ntohs(addr_client.sin_port);
+
+		cout << "## Connection is accepted from client " << client_ip << ":" <<
+				to_string(client_port) << "." << endl;
+
+		// Answer back message to client
+		string server_ip = inet_ntoa(addr_server.sin_addr);
+		int    server_port = ntohs(addr_server.sin_port);
+		string message = "Your connection request to server "+
+				server_ip+":"+to_string(server_port)+" was been accepted.\n";
+		write(socket_client, message.c_str(), message.length());
 	}
-
-	// Get the address from connected client
-	char *client_ip = inet_ntoa(addr_client.sin_addr);
-	int  client_port = ntohs(addr_client.sin_port);
-
-	cout << "## Connection is accepted from client " << client_ip << ":" <<
-			to_string(client_port) << "." << endl;
-
-	// Answer back message to client
-	string server_ip = inet_ntoa(addr_server.sin_addr);
-	int    server_port = ntohs(addr_server.sin_port);
-	string message = "Your connection request to server "+
-			server_ip+":"+to_string(server_port)+" was accepted.";
-	write(socket_client, message.c_str(), message.length());
 
 	return 0;
 }
